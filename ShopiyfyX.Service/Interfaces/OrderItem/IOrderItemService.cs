de5f0eb78283151439244f970179a4e13d32176b
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-09-17 15:59:08 +0500

    OrderItem interface added

---
+++ ShopiyfyX.Service/Interfaces/OrderItem/IOrderItemService.cs
@@ -0,0 +1,12 @@
+﻿using ShopiyfyX.Service.DTOs;
+
+namespace ShopiyfyX.Service.Interfaces.OrderItem;
+
+public interface IOrderItemService
+{
+    public Task<bool> RemoveAsync(long id);
+    public Task<List<OrderItemForResultDto>> GetAllAsync();
+    public Task<OrderItemForResultDto> GetByIdAsync(long id);
+    public Task<OrderItemForUpdateDto> UpdateAsync(OrderItemForUpdateDto dto);
+    public Task<OrderItemForResultDto> CreateAsync(OrderItemForCreationDto dto);
+}
