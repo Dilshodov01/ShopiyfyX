c23ea5fc8a8fcebc9c1b45fd975f5c028d972cb5
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-09-17 15:55:14 +0500

    Category interface added

---
+++ ShopiyfyX.Service/Interfaces/Category/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using ShopiyfyX.Service.DTOs;
+
+namespace ShopiyfyX.Service.Interfaces.Category;
+
+public interface ICategoryService
+{
+    public Task<bool> RemoveAsync(long id);
+    public Task<List<CategoryForResultDto>> GetAllAsync();
+    public Task<CategoryForResultDto> GetByIdAsync(long id);
+    public Task<CategoryForUpdateDto> UpdateAsync(CategoryForUpdateDto dto);
+    public Task<CategoryForResultDto> CreateAsync(CategoryForCreationDto dto);
+}
