23b5b6c30d4e1cb83f67f00e6ac9acc12fb81ee6
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-09-17 15:47:30 +0500

    Product interface added

---
+++ ShopiyfyX.Service/Interfaces/Product/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using ShopiyfyX.Service.DTOs;
+
+namespace ShopiyfyX.Service.Interfaces.Product;
+
+public interface IProductService
+{
+    public Task<bool> RemoveAsync(long id);
+    public Task<List<ProductForResultDto>> GetAllAsync();
+    public Task<ProductForResultDto> GetByIdAsync(long id);
+    public Task<ProductForUpdateDto> UpdateAsync(ProductForUpdateDto dto);
+    public Task<ProductForResultDto> CreateAsync(ProductForCreationDto dto);
+}
--- ShopiyfyX.Service/Interfaces/User/IUserInterface.cs
+++ ShopiyfyX.Service/Interfaces/User/IUserInterface.cs
@@ -7,6 +7,6 @@ public interface IUserInterface
     public Task<bool> RemoveAsync(long id);
     public Task<List<UserForResultDto>> GetAllAsync();
     public Task<UserForResultDto> GetByIdAsync(long id);
-    public Task<UserForResultDto> UpdateAsync(UserForUpdateDto dto);
+    public Task<UserForUpdateDto> UpdateAsync(UserForUpdateDto dto);
     public Task<UserForResultDto> CreateAsync(UserForCreationDto dto);
 }
