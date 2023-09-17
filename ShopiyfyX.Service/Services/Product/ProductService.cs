da17ee5621c2f374466af00b23bfc057d7d7d480
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-09-17 17:15:08 +0500

    ser

--- ShopiyfyX.Service/Services/Product/ProductService.cs
+++ ShopiyfyX.Service/Services/Product/ProductService.cs
@@ -1,11 +1,61 @@
-﻿using ShopiyfyX.Data.Repositories;
+﻿using ShopiyfyX.Domain.Entities;
+using ShopiyfyX.Data.Repositories;
+using ShopiyfyX.Data.IRepositories;
 using ShopiyfyX.Service.DTOs.ProductDto;
 using ShopiyfyX.Service.Interfaces.Product;
+using ShopiyfyX.Service.Exceptions;
 
-namespace ShopiyfyX.Service.Services.Product;
+namespace ShopiyfyX.Service.Services;
 
 public class ProductService : IProductService
 {
     private readonly IRepository<Product> productRepository = new Repository<Product>();
-    
+
+    public async Task<ProductForResultDto> CreateAsync(ProductForCreationDto dto)
+    {
+        var product = (await this.productRepository.SelectAllAsync()).FirstOrDefault(x => x.ProductName.ToLower() == dto.ProductName.ToLower());
+        if (product is null)
+        {
+            var products = new Product()
+            {
+                ProductName = dto.ProductName,
+                Price = dto.Price,
+                Description = dto.Description,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            var result = await productRepository.InsertAsync(product);
+
+            var mappedProduct = new ProductForResultDto()
+            {
+                Id = result.Id,
+                ProductName = result.ProductName,
+                Price = result.Price,
+                Description = result.Description,
+            };
+            return mappedProduct;
+        }
+
+        throw new ShopifyXException(400, "exit");
+    }
+
+    public Task<List<ProductForResultDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductForResultDto> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductForUpdateDto> UpdateAsync(ProductForUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
 }
--- ShopiyfyX.Service/Services/User/UserService.cs
+++ ShopiyfyX.Service/Services/User/UserService.cs
@@ -1,5 +1,37 @@
-﻿namespace ShopiyfyX.Service.Services.User;
+﻿using ShopiyfyX.Data.IRepositories;
+using ShopiyfyX.Data.Repositories;
+using ShopiyfyX.Domain.Entities;
+using ShopiyfyX.Service.DTOs.UserDto;
+using ShopiyfyX.Service.Interfaces.User;
 
-public class UserService
+namespace ShopiyfyX.Service.Services;
+
+public class UserService : IUserService
 {
+    private readonly IRepository<User> productRepository = new Repository<User>();
+
+    public Task<UserForResultDto> CreateAsync(UserForCreationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<UserForResultDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserForResultDto> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserForUpdateDto> UpdateAsync(UserForUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
 }
